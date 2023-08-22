4aaed90aba91c0f38de29dea3f61ca03f2d084fd
Adrian <reinieradrianluna@gmail.com>
2023-08-22 21:31:17 +0800

    css animation (OTP)

--- script.js
+++ script.js
@@ -17,16 +17,38 @@ function toggleNav() {
     if(overlay.classList.contains('overlay-active')) {
         // animate In-overlay
         overlay.classList.remove('overlay-slide-left');
-        overlay.classList.add('overlay-slide-right');   
+        overlay.classList.add('overlay-slide-right'); 
+        // Animate In-nav items
+        nav1.classList.remove('slide-out-1'); 
+        nav1.classList.add('slide-in-1'); 
+        nav2.classList.remove('slide-out-2'); 
+        nav2.classList.add('slide-in-2'); 
+        nav3.classList.remove('slide-3ut-1'); 
+        nav3.classList.add('slide-in-3'); 
+        nav4.classList.remove('slide-out-4'); 
+        nav4.classList.add('slide-in-4'); 
+        nav5.classList.remove('slide-out-1'); 
+        nav5.classList.add('slide-in-5'); 
     }else{
         // animate out-overlay
         overlay.classList.remove('overlay-slide-right');
         overlay.classList.add('overlay-slide-left');
+        // Animate out-nav items
+        nav1.classList.remove('slide-in-1'); 
+        nav1.classList.add('slide-out-1'); 
+        nav2.classList.remove('slide-in-2'); 
+        nav2.classList.add('slide-out-2'); 
+        nav3.classList.remove('slide-in-3'); 
+        nav3.classList.add('slide-out-3'); 
+        nav4.classList.remove('slide-in-4'); 
+        nav4.classList.add('slide-out-4'); 
+        nav5.classList.remove('slide-in-5'); 
+        nav5.classList.add('slide-out-5');
     }
 }
 // Event Listeners 
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
 nav2.addEventListener('click', toggleNav);
 nav3.addEventListener('click', toggleNav);
 nav4.addEventListener('click', toggleNav);
